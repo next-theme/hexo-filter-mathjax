09d3f7cc4f5243f6243993635dc6bec6c77c3dac
Mimi <1119186082@qq.com>
2020-01-11 13:45:43 +0800

    Remove extra `<html>` tag added by jsdom

--- index.js
+++ index.js
@@ -9,19 +9,28 @@ var config = hexo.config.mathjax = Object.assign({
 }, hexo.config.mathjax);
 
 const { mjpage } = require('mathjax-node-page');
+const fs = require('fs');
+const path = require('path');
+const css = fs.readFileSync(path.resolve(__dirname, 'style.css'), 'utf8');
 
 hexo.extend.filter.register('after_post_render', data => {
   if (!data.mathjax) return;
   return new Promise((resolve, reject) => {
     mjpage(data.content, {
       format: ["TeX"],
-      singleDollars: config.single_dollars
+      singleDollars: config.single_dollars,
+      fragment: true,
+      cssInline: false
     }, {
       cjkCharWidth: config.cjk_char_width,
       svg: config.svg
-    }, function(output) {
+    }, output => {
       data.content = output;
       resolve(data); // resulting HTML string
     });
   });
 }, 5);
+
+hexo.extend.filter.register('after_render:html', data => {
+  return data.replace(/<head>(?!<\/head>).+?<\/head>/s, str => str.replace('</head>', `<style>${css}</style></head>`));
+});
