f7583f914c7f0435bf1a13e7a3f9a63d0709dc60
Mimi <1119186082@qq.com>
2020-02-08 16:09:14 +0800

    Support MathJax v3.0

--- index.js
+++ index.js
@@ -2,34 +2,53 @@
 
 'use strict';
 
-var config = hexo.config.mathjax = Object.assign({
-  single_dollars: true,
-  cjk_char_width: 16,
-  svg: true
-}, hexo.config.mathjax);
+//
+//  Load the packages needed for MathJax
+//
+const { mathjax } = require('mathjax-full/js/mathjax.js');
+const { TeX } = require('mathjax-full/js/input/tex.js');
+const { SVG } = require('mathjax-full/js/output/svg.js');
+const { liteAdaptor } = require('mathjax-full/js/adaptors/liteAdaptor.js');
+const { RegisterHTMLHandler } = require('mathjax-full/js/handlers/html.js');
 
-const { mjpage } = require('mathjax-node-page');
-const fs = require('fs');
-const path = require('path');
-const css = fs.readFileSync(path.resolve(__dirname, 'style.css'), 'utf8');
+const { AllPackages } = require('mathjax-full/js/input/tex/AllPackages.js');
+
+//
+//  Create DOM adaptor and register it for HTML documents
+//
+const adaptor = liteAdaptor({ fontSize: 16 });
+RegisterHTMLHandler(adaptor);
+
+//
+//  Create input and output jax and a document using them on the content from the HTML file
+//
+const tex = new TeX({
+  inlineMath: { '[+]': [['$', '$']] },
+  packages: AllPackages.sort().join(', ').split(/\s*,\s*/)
+});
+const svg = new SVG({
+  fontCache: 'global', exFactor: 1 / 2
+});
 
 hexo.extend.filter.register('after_post_render', data => {
   if (!data.mathjax) return;
-  return new Promise((resolve, reject) => {
-    mjpage(data.content, {
-      format: ["TeX"],
-      singleDollars: config.single_dollars,
-      fragment: true,
-      cssInline: false
-    }, {
-      cjkCharWidth: config.cjk_char_width,
-      svg: config.svg
-    }, output => {
-      data.content = output;
-      resolve(data); // resulting HTML string
-    });
-  });
+  const html = mathjax.document(data.content, { InputJax: tex, OutputJax: svg });
+
+  //
+  //  Typeset the document
+  //
+  html.render();
+
+  //
+  //  Output the resulting HTML
+  //
+  data.content = adaptor.innerHTML(adaptor.body(html.document));
+  return data;
 }, 5);
+
+const fs = require('fs');
+const path = require('path');
+const css = fs.readFileSync(path.resolve(__dirname, 'style.css'), 'utf8');
 
 hexo.extend.filter.register('after_render:html', data => {
   return data.replace(/<head>(?!<\/head>).+?<\/head>/s, str => str.replace('</head>', `<style>${css}</style></head>`));
