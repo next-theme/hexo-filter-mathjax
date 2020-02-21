ecdf31a6613a9afaed2fa920ae8a8551dc3c240b
Mimi <1119186082@qq.com>
2020-02-21 17:32:11 +0800

    Avoid LiteAdaptor removing comments like `<!-- more -->`

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var config = hexo.config.mathjax = Object.assign({
 const { mathjax } = require('mathjax-full/js/mathjax.js');
 const { TeX } = require('mathjax-full/js/input/tex.js');
 const { SVG } = require('mathjax-full/js/output/svg.js');
-const { liteAdaptor } = require('mathjax-full/js/adaptors/liteAdaptor.js');
+const { LiteAdaptor } = require('mathjax-full/js/adaptors/liteAdaptor.js');
 const { RegisterHTMLHandler } = require('mathjax-full/js/handlers/html.js');
 
 const { AllPackages } = require('mathjax-full/js/input/tex/AllPackages.js');
@@ -22,7 +22,10 @@ const { AllPackages } = require('mathjax-full/js/input/tex/AllPackages.js');
 //
 //  Create DOM adaptor and register it for HTML documents
 //
-const adaptor = liteAdaptor({
+LiteAdaptor.prototype.value = function(node) {
+  return node.value;
+};
+const adaptor = new LiteAdaptor({
   fontSize: 16
 });
 RegisterHTMLHandler(adaptor);
