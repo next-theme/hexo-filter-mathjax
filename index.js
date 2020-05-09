3e4368adce0d72ae2ced06b8fb9493ad05ed2d3d
Johann Lee <Qinka@users.noreply.github.com>
2020-05-09 11:30:59 +0800

    Allow no css

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const config = hexo.config.mathjax = Object.assign({
   tags          : 'none',
   single_dollars: true,
   cjk_width     : 0.9,
-  normal_width  : 0.6
+  normal_width  : 0.6,
+  append_css:   true
 }, hexo.config.mathjax);
 
 const mathjax = require('./lib/filter')(config);
@@ -18,8 +19,10 @@ hexo.extend.filter.register('after_post_render', data => {
   return data;
 }, 5);
 
-const css = require('./lib/css');
+if (config.append_css) {
+  const css = require('./lib/css');
 
-hexo.extend.filter.register('after_render:html', data => {
-  return data.replace(/<head>(?!<\/head>).+?<\/head>/s, str => str.replace('</head>', `<style>${css}</style></head>`));
-});
+  hexo.extend.filter.register('after_render:html', data => {
+    return data.replace(/<head>(?!<\/head>).+?<\/head>/s, str => str.replace('</head>', `<style>${css}</style></head>`));
+  });
+}
