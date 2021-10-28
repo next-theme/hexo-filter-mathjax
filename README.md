0d3d11bf4b81bd5f229472a6c70a5fdb1887763b
XinyangLi <lixinyang411@gmail.com>
2021-10-28 11:34:31 +0800

    Add suppot of extension options in config

--- README.md
+++ README.md
@@ -33,6 +33,9 @@ mathjax:
   normal_width: 0.6 # relative normal (monospace) width
   append_css: true # add CSS to pages rendered by MathJax
   every_page: false # if true, every page will be rendered by MathJax regardless the `mathjax` setting in Front-matter
+  extension_options: {}
+    # you can put your extension options here
+    # see http://docs.mathjax.org/en/latest/options/input/tex.html#tex-extension-options for more detail
 ```
 
 ## Usage
--- test/index.js
+++ test/index.js
@@ -3,17 +3,23 @@
 require('chai').should();
 
 const config = {
-  tags          : 'none',
-  single_dollars: true,
-  cjk_width     : 0.9,
-  normal_width  : 0.6,
-  append_css    : true,
-  every_page    : false
+  tags             : 'none',
+  single_dollars   : true,
+  cjk_width        : 0.9,
+  normal_width     : 0.6,
+  append_css       : true,
+  every_page       : false,
+  extension_options: {
+    macros: {
+      Rarr: "\\Rightarrow"
+    }
+  }
 };
 
 const mathjax = require('../lib/filter')(config);
 const content = '$E=mc^2$';
 const comment = '<!-- more -->';
+const macros = "A \vee B \Rarr A"
 
 describe('MathJax', () => {
 
